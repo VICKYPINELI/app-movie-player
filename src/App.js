9fa677608108f34a095e6818e2a306e4206d9a10
vickypineli <v.pinero70@gmail.com>
2023-03-15 22:39:51 +0100

    agregar componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import { Route, Router } from 'react-router-dom';
-import './App.css';
-import MoviesGrid from './components/MoviesGrid';
+import styles from "./App.module.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { MovieDetails } from "./pages/MovieDetails";
+import { LandingPage } from "./pages/LandingPage";
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <h1 className={"App-title"}>APP PLAYER MOVIES</h1>
-      </header>
-      <main>
-        <MoviesGrid/>
-        <Router>
-            <Route></Route>
-            <Route></Route>
-            <Route></Route>
-            <Route></Route>
-            <Route></Route>
-        </Router>
-      </main>
-    </div>
+    <div  className="App">
+    <Router >
+        <header className="App-header">
+          <Link  to="/">
+              <h1 className={styles.title}>Movies</h1>
+          </Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/movies/:movieId" element={<MovieDetails/>} />
+            <Route path="/" element={<LandingPage />} />
+            {/* <Route path="*" element={<Navigate replace to="/" />} /> */}
+          </Routes>
+        </main>
+     </Router>
+     </div>
   );
 }
 
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,20 +1,22 @@
-import styles from "./MovieCard.css";
+import { Link } from "react-router-dom";
+import styles from "./MovieCard.module.css";
+import { getMovieImg } from "../utils/getMovieImg";
 
-
-function MovieCard({ movie }) {
-  const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
+export function MovieCard({ movie }) {
+  const imageUrl = getMovieImg(movie.poster_path, 300);
   return (
-    <li className="movieCard">
-      <img
-        width={230}
-        height={345}
-        className="MovieIMage"
-        src={imageUrl}
-        alt={movie.title}
-      />
-      <div className="movieTitle">{movie.title}</div>
+    <li className={styles.movieCard}>
+      <Link to={"/movies/" + movie.id}>
+        <img
+          width={230}
+          height={345}
+          className={styles.movieImage}
+          src={imageUrl}
+          alt={movie.title}
+        />
+        <div>{movie.title}</div>
+      </Link>
     </li>
   );
-};
-export default MovieCard;
+}
 
